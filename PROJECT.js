a7335f2cc2c962f52c471ec0ea1b2c43dd93834b
Adam Mechaeil <65824352+AdamMechaeil@users.noreply.github.com>
2020-09-09 17:17:50 +0530

    Update PROJECT.js

--- PROJECT.js
+++ PROJECT.js
@@ -1,28 +1,40 @@
 let screen = document.getElementById('screen');
+let result = document.getElementById('result');
+let resultValue = '';
 buttons = document.querySelectorAll('button');
 let screenValue = '';
-let equalPressed = false;
+equalPresed = false;
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         if (e.target.value == "C") {
+            resultValue = '';
+            result.value = resultValue;
             screenValue = '';
             screen.value = screenValue;
 
+        } else if (e.target.value == "dlt") {
+            screenValue = screenValue.substring(0, screenValue.length - 1);
+            screen.value = screenValue;
         } else if (e.target.value == "=") {
-            screen.value = eval(screenValue);
-            equalPressed = true;
-
-        } else if (equalPressed == true) {
+            result.value = eval(screenValue);
+            equalPresed = true;
+        } else if (equalPresed && !(e.target.value == "+" || "-" || '*' || "/")) {
+            resultValue = '';
+            result.value = resultValue;
             screenValue = '';
             screen.value = screenValue;
             screenValue += e.target.value;
             screen.value = screenValue;
-            equalPressed = false;
+            equalPresed = false;
+        } else if (equalPresed && (e.target.value == "+" || "-" || '*' || "/")) {
+            screenValue += e.target.value;
+            screen.value = screenValue;
         } else {
             screenValue += e.target.value;
             screen.value = screenValue;
+            equalPresed = false;
         }
 
 
-    })
+    });
 }
